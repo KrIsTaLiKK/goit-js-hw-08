db8d81cc95c93def597ad76d77c8259f1b873541
Krystyna <zkristal95@gmail.com>
2023-11-17 12:53:03 -0300

    made 1-st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.js-gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+    </li>`;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.js-gallery .gallery__link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  scrollZoom: true,
+});
