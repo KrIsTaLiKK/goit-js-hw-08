2dd2c58c8f98b302fcddfdfb8d822f7ca5262251
Krystyna <zkristal95@gmail.com>
2023-11-18 20:30:53 -0300

    made 2-nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const currentTimeOfVideoplayer = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTimeOfVideoplayer);
+  }, 1000)
+);
+
+const getCurrentTimeOfVideoplayer = Number(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+player.setCurrentTime(getCurrentTimeOfVideoplayer);
