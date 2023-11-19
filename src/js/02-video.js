d9e6937c77b4a598b943c0f6968e3bd9ffbef142
Krystyna <zkristal95@gmail.com>
2023-11-19 15:19:20 -0300

    made 3-rd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
 const player = new Player('vimeo-player');
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    const currentTimeOfVideoplayer = data.seconds;
-    localStorage.setItem('videoplayer-current-time', currentTimeOfVideoplayer);
-  }, 1000)
-);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-const getCurrentTimeOfVideoplayer = Number(
-  localStorage.getItem('videoplayer-current-time')
-);
+function onPlay(data) {
+  const currentTimeOfVideoplayer = data.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTimeOfVideoplayer);
+}
+
+const getCurrentTimeOfVideoplayer = Number(localStorage.getItem(STORAGE_KEY));
 
 player.setCurrentTime(getCurrentTimeOfVideoplayer);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[name="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormData, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormData(evt) {
+  const name = evt.target.name;
+  const value = evt.target.value;
+
+  formData[name] = value;
+  const formDataJSON = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, formDataJSON);
+}
+
+function populateForm() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedFormData) {
+    const { email, message } = savedFormData;
+    emailEl.value = email;
+    messageEl.value = message;
+  }
+}
