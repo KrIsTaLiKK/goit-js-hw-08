86c87d8d61391ad22db77a95b6de0b13f44973c7
Krystyna <zkristal95@gmail.com>
2023-11-21 14:55:45 -0300

    fixed 3-rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,36 +5,41 @@ const emailEl = document.querySelector('input[name="email"]');
 const messageEl = document.querySelector('textarea[name="message"]');
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('input', throttle(onFormDataWriteToLS, 500));
 
-const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
+const FORM_KEY = 'feedback-form-state';
+
+function onGetFormData() {
+  const formData = {};
+  formData.email = form.elements.email.value;
+  formData.message = form.elements.message.value;
+  return formData;
+}
+
+function onFormDataWriteToLS() {
+  localStorage.setItem(FORM_KEY, JSON.stringify(onGetFormData()));
+}
 
 populateForm();
 
-function onFormSubmit(evt) {
-  evt.preventDefault();
-  form.reset();
-  localStorage.removeItem(STORAGE_KEY);
-
-  console.log(formData);
-}
-
-function onFormData() {
-  formData = {
-    email: form.elements.email.value,
-    message: form.elements.message.value,
-  };
-
-  const formDataJSON = JSON.stringify(formData);
-  localStorage.setItem(STORAGE_KEY, formDataJSON);
-}
-
 function populateForm() {
-  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const savedFormData = JSON.parse(localStorage.getItem(FORM_KEY));
 
   if (savedFormData) {
-    emailEl.value = savedFormData.email;
-    messageEl.value = savedFormData.message;
+    const { email, message } = savedFormData;
+    emailEl.value = email;
+    messageEl.value = message;
   }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  if (!emailEl.value || !messageEl.value) {
+    return;
+  }
+
+  console.log(onGetFormData());
+  form.reset();
+  localStorage.removeItem(FORM_KEY);
 }
